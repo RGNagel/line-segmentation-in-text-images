6d48f2257d5ca67f538448394c6bf123dbbb9d67
Rafael Nagel <rafael.gustavo.nagel@gmail.com>
2020-10-11 09:15:54 -0300

    add Rectangle class; count chars in each line

--- TextImage.py
+++ TextImage.py
@@ -86,8 +86,6 @@ class TextImage:
 
     def getTextLines(self):
 
-        th = 0.1 # 0 to 1
-
         text_lines = 0
         row_ctr = 0
 
@@ -113,7 +111,7 @@ class TextImage:
                 # a text line was identified
                 text_lines = text_lines + 1
                 # save line coordinates for future operations
-                self.lines.append(TextLineImage(start=(row - row_ctr, 0), end=(row, self.cols - 1)))
+                self.lines.append(TextLineImage(start=(row - row_ctr, 0), end=(row - 1, self.cols - 1)))
                 row_ctr = 0
 
 
@@ -121,11 +119,24 @@ class TextImage:
 
         return text_lines
 
+    def getCharsPerLine(self):
+        charsPerLine = []
+        for line in self.lines:
+            charsPerLine.append(line.getChars(self))
+
+        return charsPerLine
 
     def drawTextLines(self):
 
         for line in self.lines:
-            line.drawPerimeter(self.image_rgb)
+            # yellow
+            line.drawPerimeter(self.image_rgb, color=(255, 255, 0))
+
+    def drawChars(self):
+
+        for line in self.lines:
+            # red
+            line.drawChars(self.image_rgb, color=(255, 0, 0))
 
     def saveImages(self):
 
@@ -137,26 +148,77 @@ class TextImage:
         filepath_bin = self.filepath.split('.')[0] + '_bin.' + self.filepath.split('.')[1]
         io.imsave(filepath_bin, img_as_ubyte(self.image_bin))
 
-    # @staticmethod
-    # def drawRect(image, start, end):
 
-    #     # rectangle_perimeter generates coordinates of pixels that are exactly around a rectangle
-    #     r, c = rectangle_perimeter(start, end=end, shape=image.shape)
-        
-    #     # image[r, c] = (255, 255, 0)
-    #     # or set_color(image, (r,c), (255,255,0))
-    #     set_color(image, (r,c), (255, 255, 0))
-
-    
-class TextLineImage:
+class Rectangle:
 
     def __init__(self, start, end):
         self.start = start
         self.end   = end
 
-    def drawPerimeter(self, image):
+        self.row_len  = self.end[0] - self.start[0] + 1
+        self.col_len  = self.end[1] - self.start[1] + 1
+
+        assert self.row_len > 0, "row length must be greater than 0"
+        assert self.col_len > 0, "col length must be greater than 0"
+
+
+    def drawPerimeter(self, image, color):
+
+        if len(color) != 3:
+            raise ValueError("color must be of size 3")
+
+        if image[0][0].size != 3:
+            raise ValueError("image must be of size 3")
+
         r, c = rectangle_perimeter(self.start, end=self.end, shape=image.shape)
-        set_color(image, (r, c), (255, 255, 0))
+        set_color(image, (r, c), color)
         # image[r, c] = (255, 255, 0)
 
-        
+
+class TextLineImage(Rectangle):
+
+    chars = []
+
+    def getChars(self, text_image: TextImage):
+
+        image            = text_image.image_bin
+        text_color       = text_image.text_color
+        background_color = text_image.background_color
+
+        if image[0][0].size != 1:
+            raise ValueError('image must be one-dimension')
+
+        threshold_percent_px_rows = 0.05
+        threshold_qty_px_cols_is_char = 3
+
+        chars = 0
+        col_ctr = 0
+
+        for col in range(self.start[1], self.end[1]):
+
+            text_pxs = 0
+            background_pxs = 0
+
+            for row in range(self.start[0], self.end[0]):
+
+                if image[row][col] == text_color:
+                    text_pxs = text_pxs + 1
+                else:
+                    background_pxs = background_pxs + 1
+
+            if text_pxs / self.row_len >= threshold_percent_px_rows:
+                col_ctr = col_ctr + 1
+
+            elif col_ctr >= threshold_qty_px_cols_is_char:
+                chars = chars + 1
+                self.chars.append(Rectangle(start=(self.start[0], col - col_ctr),end=(self.end[0], col - 1)))
+                col_ctr = 0
+
+        return chars
+
+    def drawChars(self, image, color):
+
+        for char in self.chars:
+            # red
+            char.drawPerimeter(image, color)
+            
--- testing.py
+++ testing.py
@@ -11,7 +11,9 @@ print("bg color : ", img_obj.background_color)
 print("txt color: ", img_obj.text_color)
 
 print("text lines qty: ", img_obj.getTextLines())
-
 img_obj.drawTextLines()
+
+print("chars in each line:", img_obj.getCharsPerLine())
+img_obj.drawChars()
 
 img_obj.saveImages()
